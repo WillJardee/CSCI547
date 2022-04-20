40ac028470c5daeec353fe66336f51c12a8cdc27
WillJardee <willjardee@gmail.com>
2022-04-19 18:17:55 -0600

    updated dataset

--- code/main.py
+++ code/main.py
@@ -1,9 +1,12 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 
 def build_dataset(dataset):
     dat = np.genfromtxt("../datasets/" + dataset + "/data.csv", delimiter=",", dtype=str)
     X, y = dat[:, 0:-1], dat[:, -1]
+    enc = OneHotEncoder(handle_unknown='ignore')
+
     rf = RandomForestClassifier(n_estimators=1000, random_state=42)
     rf.fit(X, y)
     pass
