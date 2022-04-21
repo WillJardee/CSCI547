e513cdca6c259ef5d133b250c77022adb526056c
WillJardee <willjardee@gmail.com>
2022-04-20 20:12:31 -0600

    added tree medling

---
+++ code/RuleExtractor.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+
+class LorentzMap:
+    def __int__(self, num_pars, num_classes):
+        self.map = np.zeros((num_pars*2, num_pars*2))
+        self.n_pars = num_pars
+        self.n_class = num_classes
+        self.n_tot = num_pars + num_classes
+
+    def add_term(self, x):
+        counts = []
+        for i in range(len(x)):
+            if x[i] == -1: continue
+            counts.append(x[i] if x[i] == 1 else x[i]+self.n_tot)
+        counts = [(x, y) for x in counts for y in counts]
+        for i in counts: self.map[i] += 1
+
+
+
--- code/main.py
+++ code/main.py
@@ -1,16 +1,34 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
+from sklearn import tree as Tree
 import numpy as np
+import TreeRuleExtractor as tRule
+
 
 def build_dataset(dataset):
     dat = np.genfromtxt("../datasets/" + dataset + "/data.csv", delimiter=",", dtype=str)
     X, y = dat[:, 0:-1], dat[:, -1]
     enc = OneHotEncoder(handle_unknown='ignore')
+    x_hot = enc.fit_transform(X)
+    # ency = OneHotEncoder(handle_unknown='ignore')
+    # ency.fit(['unacc', 'acc', 'good', 'vgood'])
 
-    rf = RandomForestClassifier(n_estimators=1000, random_state=42)
-    rf.fit(X, y)
-    pass
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+    return rf.fit(x_hot, y)
 
 
 if __name__ == '__main__':
-    pass
+    rf = build_dataset('car')
+    # tRule.tree_to_code(rf[0], [str(i) + "." for i in list(range(27))])
+    rules = tRule.get_rules(rf[0], [str(i) for i in list(range(27))], ['unacc', 'acc', 'good', 'vgood'])
+    for r in rules:
+        print(r)
+    # rule = tRule.tree_to_code2(rf[50])
+    # arr = np.zeros(21)
+    # print(rule[10])
+    # for i in rule[10][:-1]:
+    #     arr[i[0]-1] = i[1]
+    # print(arr)
+    # print(rf[50].predict([arr]))
+    # arr = np.concatenate((arr, ency.transform(rf[50].predict([arr]).reshape(1, -1))))
+    # print(arr)
